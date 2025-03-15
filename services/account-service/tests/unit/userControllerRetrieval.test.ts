4e65872bdb8247f3f9c0a82b97001c3dc820b6ab
geozi <yorgosaz@gmail.com>
2025-03-15 18:57:14 +0200

    test: Added userControllerRetrieval test suite.

---
+++ services/account-service/tests/unit/userControllerRetrieval.test.ts
@@ -0,0 +1,56 @@
+import { User } from "models/user.model";
+import sinon, { SinonSpy, SinonStub } from "sinon";
+import { validUserInput } from "../testInputs";
+import { Response } from "express";
+import { IRequest } from "interfaces/secondary/iRequest.interface";
+import { retrieveUser } from "controllers/user.controller";
+import assert from "assert";
+import { httpCodes } from "resources/codes/responseStatusCodes";
+import { userControllerResponseMessages } from "messages/response/userControllerResponse.message";
+
+describe("User controller retrieval unit tests", () => {
+  let req: Partial<IRequest>;
+  let res: Partial<Response>;
+  let statusStub: SinonStub;
+  let jsonSpy: SinonSpy;
+  const mockUser = new User(validUserInput);
+
+  describe("Positive scenario(s)", () => {
+    beforeEach(() => {
+      sinon.restore();
+      res = {
+        status: sinon.stub().callsFake(() => {
+          return res;
+        }) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            username: validUserInput.username,
+            email: validUserInput.email,
+            password: validUserInput.password,
+          })
+        ),
+        user: mockUser,
+      };
+    });
+
+    it("request has valid inputs", async () => {
+      retrieveUser(req as IRequest, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(statusStub.calledWith(httpCodes.OK), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: userControllerResponseMessages.USER_RETRIEVED,
+          data: mockUser,
+        }),
+        true
+      );
+    });
+  });
+});
