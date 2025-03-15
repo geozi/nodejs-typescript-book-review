beb04daa2f2f7273e9a84f03210f60467d1197a0
geozi <yorgosaz@gmail.com>
2025-03-15 13:08:14 +0200

    feat: Added new message to userControllerResponse messages and try/catch block to updateUser in user.repository.

--- services/account-service/src/messages/response/userControllerResponse.message.ts
+++ services/account-service/src/messages/response/userControllerResponse.message.ts
@@ -2,4 +2,5 @@ export const userControllerResponseMessages = {
   USER_REGISTERED: "Successful user registration",
   USER_UPDATED: "Successful user update",
   USER_RETRIEVED: "Successful user retrieval",
+  USER_NOT_FOUND: "User was not found",
 };
