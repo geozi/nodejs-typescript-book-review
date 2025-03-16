2fb68dc7f9f87533fe40863c4df43e693e7c9d77
geozi <yorgosaz@gmail.com>
2025-03-16 18:13:56 +0200

    test: Added personUpdateRules test suite.

---
+++ services/account-service/tests/backendIntegration/personUpdateRules.test.ts
@@ -0,0 +1,494 @@
+import { IRequest } from "interfaces/secondary/iRequest.interface";
+import sinon, { SinonSpy, SinonStub } from "sinon";
+import {
+  invalidAddressInputs,
+  invalidObjectIdInputs,
+  invalidPersonInputs,
+  validPersonInput,
+  validUserInput,
+} from "../../tests/testInputs";
+import { personInfoUpdateRules } from "middleware/person.rule";
+import { catchExpressValidationErrors } from "middleware/expressError.catch";
+import { Response } from "express";
+import { User } from "models/user.model";
+import assert from "assert";
+import { httpCodes } from "resources/codes/responseStatusCodes";
+import { commonResponseMessages } from "messages/response/commonResponse.message";
+import { personFailedValidation } from "messages/validation/personValidation.message";
+import { addressFailedValidation } from "messages/validation/addressValidation.message";
+
+describe.only("Person update rules integration tests", () => {
+  let req: Partial<IRequest>;
+  let res: Partial<Response>;
+  let next: SinonSpy;
+  let statusStub: SinonStub;
+  let jsonSpy: SinonSpy;
+  const mockId = "67d6f5421100cfe33701b06e";
+  const mockUser = new User(validUserInput);
+  const updateRulesArray = [
+    ...personInfoUpdateRules(),
+    catchExpressValidationErrors,
+  ];
+
+  describe("Positive scenario(s)", () => {
+    beforeEach(() => {
+      sinon.restore();
+      res = {
+        status: sinon.stub().callsFake(() => {
+          return res;
+        }) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      next = sinon.spy();
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            id: mockId,
+            firstName: validPersonInput.firstName,
+            lastName: validPersonInput.lastName,
+            ssn: validPersonInput.ssn,
+            phoneNumber: validPersonInput.phoneNumber,
+            address: validPersonInput.address,
+          })
+        ),
+        user: mockUser,
+      };
+    });
+
+    it("request has valid inputs", async () => {
+      for (const middleware of updateRulesArray) {
+        await middleware(req as IRequest, res as Response, next);
+      }
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(statusStub.notCalled, true);
+      assert.strictEqual(jsonSpy.notCalled, true);
+    });
+  });
+
+  describe("Negative scenarios", () => {
+    describe("bad requests (400)", () => {
+      beforeEach(() => {
+        sinon.restore();
+        res = {
+          status: sinon.stub().callsFake(() => {
+            return res;
+          }) as unknown as SinonStub,
+          json: sinon.spy(),
+        };
+
+        next = sinon.spy();
+        req = {
+          body: JSON.parse(
+            JSON.stringify({
+              id: mockId,
+              firstName: validPersonInput.firstName,
+              lastName: validPersonInput.lastName,
+              ssn: validPersonInput.ssn,
+              phoneNumber: validPersonInput.phoneNumber,
+              address: validPersonInput.address,
+            })
+          ),
+          user: mockUser,
+        };
+      });
+
+      it("id is undefined", async () => {
+        req.body.id = undefined;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              { message: personFailedValidation.PERSON_ID_REQUIRED_MESSAGE },
+            ],
+          }),
+          true
+        );
+      });
+
+      invalidObjectIdInputs.OBJECT_ID_LENGTH_CASES.forEach(
+        ([testName, invalidLengthId]) => [
+          it(testName, async () => {
+            req.body.id = invalidLengthId;
+
+            for (const middleware of updateRulesArray) {
+              await middleware(req as IRequest, res as Response, next);
+            }
+
+            statusStub = res.status as SinonStub;
+            jsonSpy = res.json as SinonSpy;
+
+            assert.strictEqual(
+              statusStub.calledWith(httpCodes.BAD_REQUEST),
+              true
+            );
+            assert.strictEqual(
+              jsonSpy.calledWith({
+                message: commonResponseMessages.BAD_REQUEST,
+                errors: [
+                  {
+                    message:
+                      personFailedValidation.PERSON_ID_OUT_OF_LENGTH_MESSAGE,
+                  },
+                ],
+              }),
+              true
+            );
+          }),
+        ]
+      );
+
+      invalidObjectIdInputs.OBJECT_ID_INVALID_CASES.forEach(
+        ([testName, invalidId]) => {
+          it(testName, async () => {
+            req.body.id = invalidId;
+
+            for (const middleware of updateRulesArray) {
+              await middleware(req as IRequest, res as Response, next);
+            }
+
+            statusStub = res.status as SinonStub;
+            jsonSpy = res.json as SinonSpy;
+
+            assert.strictEqual(
+              statusStub.calledWith(httpCodes.BAD_REQUEST),
+              true
+            );
+            assert.strictEqual(
+              jsonSpy.calledWith({
+                message: commonResponseMessages.BAD_REQUEST,
+                errors: [
+                  { message: personFailedValidation.PERSON_ID_INVALID_MESSAGE },
+                ],
+              }),
+              true
+            );
+          });
+        }
+      );
+
+      it("firstName is too short", async () => {
+        req.body.firstName = invalidPersonInputs.TOO_SHORT_FIRST_NAME;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message:
+                  personFailedValidation.FIRST_NAME_BELOW_MIN_LENGTH_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("firstName is invalid", async () => {
+        req.body.firstName = invalidPersonInputs.INVALID_FIRST_NAME;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message: personFailedValidation.FIRST_NAME_INVALID_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("lastName is too short", async () => {
+        req.body.lastName = invalidPersonInputs.TOO_SHORT_LAST_NAME;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message:
+                  personFailedValidation.LAST_NAME_BELOW_MIN_LENGTH_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("lastName is invalid", async () => {
+        req.body.lastName = invalidPersonInputs.INVALID_LAST_NAME;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message: personFailedValidation.LAST_NAME_INVALID_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      invalidPersonInputs.INVALID_SSN_CASES.forEach(
+        ([testName, invalidSsn]) => {
+          it(testName, async () => {
+            req.body.ssn = invalidSsn;
+
+            for (const middleware of updateRulesArray) {
+              await middleware(req as IRequest, res as Response, next);
+            }
+
+            statusStub = res.status as SinonStub;
+            jsonSpy = res.json as SinonSpy;
+
+            assert.strictEqual(
+              statusStub.calledWith(httpCodes.BAD_REQUEST),
+              true
+            );
+            assert.strictEqual(
+              jsonSpy.calledWith({
+                message: commonResponseMessages.BAD_REQUEST,
+                errors: [
+                  {
+                    message: personFailedValidation.SSN_INVALID_MESSAGE,
+                  },
+                ],
+              }),
+              true
+            );
+          });
+        }
+      );
+
+      invalidPersonInputs.INVALID_PHONE_NUMBER_CASES.forEach(
+        ([testName, invalidPhoneNumber]) => {
+          it(testName, async () => {
+            req.body.phoneNumber = invalidPhoneNumber;
+
+            for (const middleware of updateRulesArray) {
+              await middleware(req as IRequest, res as Response, next);
+            }
+
+            statusStub = res.status as SinonStub;
+            jsonSpy = res.json as SinonSpy;
+
+            assert.strictEqual(
+              statusStub.calledWith(httpCodes.BAD_REQUEST),
+              true
+            );
+            assert.strictEqual(
+              jsonSpy.calledWith({
+                message: commonResponseMessages.BAD_REQUEST,
+                errors: [
+                  {
+                    message:
+                      personFailedValidation.PHONE_NUMBER_INVALID_MESSAGE,
+                  },
+                ],
+              }),
+              true
+            );
+          });
+        }
+      );
+
+      it("address.streetName is too short", async () => {
+        req.body.address.streetName =
+          invalidAddressInputs.TOO_SHORT_STREET_NAME;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message:
+                  addressFailedValidation.STREET_NAME_BELOW_MIN_LENGTH_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("address.residenceNumber is invalid", async () => {
+        req.body.address.residenceNumber =
+          invalidAddressInputs.INVALID_RESIDENCE_NUMBER;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message:
+                  addressFailedValidation.RESIDENCE_NUMBER_INVALID_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("address.residenceNumber is negative", async () => {
+        req.body.address.residenceNumber =
+          invalidAddressInputs.NEGATIVE_RESIDENCE_NUMBER;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message:
+                  addressFailedValidation.RESIDENCE_NUMBER_NEGATIVE_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("address.zipCode is out of length", async () => {
+        req.body.address.zipCode = invalidAddressInputs.ZIP_CODE_OUT_OF_LENGTH;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message: addressFailedValidation.ZIP_CODE_OUT_OF_LENGTH_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("address.zipCode is invalid", async () => {
+        req.body.address.zipCode = invalidAddressInputs.INVALID_ZIP_CODE;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message: addressFailedValidation.ZIP_CODE_INVALID_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+
+      it("address.city is invalid", async () => {
+        req.body.address.city = invalidAddressInputs.INVALID_CITY;
+
+        for (const middleware of updateRulesArray) {
+          await middleware(req as IRequest, res as Response, next);
+        }
+
+        statusStub = res.status as SinonStub;
+        jsonSpy = res.json as SinonSpy;
+
+        assert.strictEqual(statusStub.calledWith(httpCodes.BAD_REQUEST), true);
+        assert.strictEqual(
+          jsonSpy.calledWith({
+            message: commonResponseMessages.BAD_REQUEST,
+            errors: [
+              {
+                message: addressFailedValidation.CITY_INVALID_MESSAGE,
+              },
+            ],
+          }),
+          true
+        );
+      });
+    });
+  });
+});
