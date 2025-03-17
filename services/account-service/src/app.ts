5cb7bedd622232ce5f2fc60cbc05c4e4a71f6683
geozi <yorgosaz@gmail.com>
2025-03-17 19:14:23 +0200

    build: Added dev script command to package.json and connection details for database and redis server to app.ts.

--- services/account-service/src/app.ts
+++ services/account-service/src/app.ts
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import { redisClient } from "../redis.config";
+dotenv.config();
+const app = express();
+const port = 3000;
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Connection to database established");
+
+    await redisClient.connect();
+  } catch (error) {
+    console.log("Failed to connect to database", error);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+connectToDb();
