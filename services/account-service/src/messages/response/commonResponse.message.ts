5aca9f36cfcd1f5cca12f80def83a5e105cbf174
geozi <yorgosaz@gmail.com>
2025-03-19 19:17:06 +0200

    docs: Added typedocs to src/messages/response.

--- services/account-service/src/messages/response/commonResponse.message.ts
+++ services/account-service/src/messages/response/commonResponse.message.ts
@@ -1,6 +1,39 @@
+/**
+ * Common HTTP response messages.
+ * @module src/messages/response/commonResponse.message
+ */
+
+/**
+ * Contains HTTP response messages sent by all controllers.
+ *
+ * @type {object}
+ * @property {string} BAD_REQUEST - Message sent when an HTTP request is invalid.
+ * @property {string} SERVER_ERROR - Message sent when an internal server error occurs.
+ * @property {string} REDIS_ERROR - Message sent when a redis server error occurs.
+ * @property {string} INVALID_JSON - Message sent when the json payload of an HTTP request is invalid.
+ */
 export const commonResponseMessages = {
+  /**
+   * Message sent when an HTTP request is invalid.
+   * @type {string}
+   */
   BAD_REQUEST: "Bad request",
+
+  /**
+   * Message sent when an internal server error occurs.
+   * @type {string}
+   */
   SERVER_ERROR: "Internal server error",
+
+  /**
+   * Message sent when a redis server error occurs.
+   * @type {string}
+   */
   REDIS_ERROR: "Redis server error",
+
+  /**
+   * Message sent when the json payload of an HTTP request is invalid.
+   * @type {string}
+   */
   INVALID_JSON: "Invalid JSON payload",
 };
--- services/account-service/src/messages/response/userControllerResponse.message.ts
+++ services/account-service/src/messages/response/userControllerResponse.message.ts
@@ -1,6 +1,39 @@
+/**
+ * User controller HTTP response messages.
+ * @module src/messages/response/userControllerResponse.message
+ */
+
+/**
+ * Contains HTTP response messages sent by the user controller.
+ *
+ * @type {object}
+ * @property {string} USER_REGISTERED - Message sent when a new user is created and successfully added to database.
+ * @property {string} USER_UPDATED - Message sent when an existing user is successfully updated.
+ * @property {string} USER_RETRIEVED - Message sent when a user is successfully retrieved from database.
+ * @property {string} USER_NOT_FOUND - Message sent when the requested user is not found.
+ */
 export const userControllerResponseMessages = {
+  /**
+   * Message sent when a new user is created and successfully added to database.
+   * @type {string}
+   */
   USER_REGISTERED: "Successful user registration",
+
+  /**
+   * Message sent when an existing user is successfully updated.
+   * @type {string}
+   */
   USER_UPDATED: "Successful user update",
+
+  /**
+   * Message sent when a user is successfully retrieved from database.
+   * @type {string}
+   */
   USER_RETRIEVED: "Successful user retrieval",
+
+  /**
+   * Message sent when the requested user is not found.
+   * @type {string}
+   */
   USER_NOT_FOUND: "User was not found",
 };
