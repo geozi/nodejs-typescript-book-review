4b842cbacb5fa339b4e2b4d53a0f27788bea275f
geozi <yorgosaz@gmail.com>
2025-03-19 18:05:37 +0200

    docs: Added typedocs to src/errors.

--- services/account-service/src/errors/notFoundError.class.ts
+++ services/account-service/src/errors/notFoundError.class.ts
@@ -1,7 +1,19 @@
+/**
+ * NotFoundError class.
+ * @module src/errors/notFoundError.class
+ */
 import { httpCodes } from "resources/codes/responseStatusCodes";
 import { AbstractError } from "./abstractError.class";
 
+/**
+ * Triggered when the requested document(s) is/are not found (404).
+ * @extends {AbstractError}
+ */
 export class NotFoundError extends AbstractError {
+  /**
+   * Creates an instance of NotFoundError.
+   * @param {string} message The error message.
+   */
   constructor(message: string) {
     super(httpCodes.NOT_FOUND, message);
     this.name = "NotFoundError";
