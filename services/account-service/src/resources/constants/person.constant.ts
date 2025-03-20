2e0a5d28bb6d0403511e542f060b37b1e5a6b385
geozi <yorgosaz@gmail.com>
2025-03-20 12:01:32 +0200

    docs: Added typedocs to src/resources/constants.

--- services/account-service/src/resources/constants/person.constant.ts
+++ services/account-service/src/resources/constants/person.constant.ts
@@ -1,4 +1,25 @@
+/**
+ * Person constants.
+ * @module src/resources/constants/person.constant
+ */
+
+/**
+ * Contains numeric constants used in the person model and in express-validator operations.
+ *
+ * @type {object}
+ * @property {number} NAME_MIN_LENGTH - The minimum length accepted for a first name and a last name.
+ * @property {number} SSN_LENGTH - The accepted length for a social security number.
+ */
 export const personConstants = {
+  /**
+   * The minimum length accepted for a first name and a last name.
+   * @type {number}
+   */
   NAME_MIN_LENGTH: 2,
+
+  /**
+   * The accepted length for a social security number.
+   * @type {number}
+   */
   SSN_LENGTH: 11,
 };
