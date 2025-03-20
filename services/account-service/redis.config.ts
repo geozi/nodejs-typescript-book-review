300525cd2aec30e2e517ec0a5709e9a7f7d4cdc5
geozi <yorgosaz@gmail.com>
2025-03-20 18:23:29 +0200

    fix: Changed url address in redisClient.

--- services/account-service/redis.config.ts
+++ services/account-service/redis.config.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-export const redisClient = createClient();
+export const redisClient = createClient({
+  url: "redis://redis:6379",
+});
 
 redisClient.on("error", (err) => console.log("Redis client error:", err));
 redisClient.on("connect", () =>
