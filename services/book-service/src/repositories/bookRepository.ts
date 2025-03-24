fc0e0e195351de955fc0168dd114af5b149a7aee
geozi <yorgosaz@gmail.com>
2025-03-24 17:56:01 +0200

    feat: Added try/catch block to getBookByTitle function in bookRepository.

--- services/book-service/src/repositories/bookRepository.ts
+++ services/book-service/src/repositories/bookRepository.ts
@@ -1,11 +1,30 @@
+import { ValidationError } from "class-validator";
 import { AppDataSource } from "config/dataSource";
 import { Book } from "entities/Book";
 import { IBookUpdate } from "interfaces/IBookUpdate";
+import { appLogger } from "../../logs/loggerConfigs";
+import { ServerError } from "errors/serverErrorClass";
 
 const bookRepository = AppDataSource.getRepository(Book);
 
-export const getBookByTitle = async (title: string) => {
-  return await bookRepository.findOneBy({ title: title });
+export const getBookByTitle = async (title: string): Promise<Book | null> => {
+  try {
+    return await bookRepository.findOneBy({ title: title });
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      appLogger.error(
+        `Book repository: ${getBookByTitle.name} -> ValidationError detected and re-thrown`
+      );
+
+      throw error;
+    }
+
+    appLogger.error(
+      `Book repository: ${getBookByTitle.name} -> ServerError thrown`
+    );
+
+    throw new ServerError("");
+  }
 };
 
 export const getBookById = async (id: number) => {
