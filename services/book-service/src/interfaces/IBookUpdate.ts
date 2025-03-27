cd790b78c9176a51436d1cf92cf527fda53f6aef
geozi <yorgosaz@gmail.com>
2025-03-27 13:01:03 +0200

    refactor: Removed relation fields in IAuthorUpdate and IBookUpdate interfaces.

--- services/book-service/src/interfaces/IBookUpdate.ts
+++ services/book-service/src/interfaces/IBookUpdate.ts
@@ -1,10 +1,6 @@
-import { Author } from "entities/Author";
-import { Edition } from "entities/Edition";
 import { Genre } from "resources/enum/Genre";
 
 export interface IBookUpdate {
   title?: string;
   genre?: Genre;
-  authors?: Author[];
-  editions?: Edition[];
 }
