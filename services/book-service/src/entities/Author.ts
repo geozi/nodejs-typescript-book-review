09c4180e139acce77242bef7bd8bf5dffc75a480
geozi <yorgosaz@gmail.com>
2025-03-27 16:54:21 +0200

    feat: Added custom validation error messages for @IsString decorator.

--- services/book-service/src/entities/Author.ts
+++ services/book-service/src/entities/Author.ts
@@ -12,22 +12,22 @@ export class Author {
   id!: number;
 
   @Column({ type: "varchar" })
-  @IsString()
+  @IsString({ message: authorFailedValidation.FIRST_NAME_NOT_STRING_MESSAGE })
   @MinLength(authorConstants.NAME_MIN_LENGTH, {
     message: authorFailedValidation.FIRST_NAME_BELOW_MIN_LENGTH_MESSAGE,
   })
   @Matches(ALPHABETIC_REGEX, {
-    message: authorFailedValidation.FIRST_NAME_INVALID,
+    message: authorFailedValidation.FIRST_NAME_INVALID_MESSAGE,
   })
   first_name!: string;
 
   @Column({ type: "varchar" })
-  @IsString()
+  @IsString({ message: authorFailedValidation.LAST_NAME_NOT_STRING_MESSAGE })
   @MinLength(authorConstants.NAME_MIN_LENGTH, {
     message: authorFailedValidation.LAST_NAME_BELOW_MIN_LENGTH_MESSAGE,
   })
   @Matches(ALPHABETIC_REGEX, {
-    message: authorFailedValidation.LAST_NAME_INVALID,
+    message: authorFailedValidation.LAST_NAME_INVALID_MESSAGE,
   })
   last_name!: string;
 
--- services/book-service/src/messages/validation/authorValidationMessages.ts
+++ services/book-service/src/messages/validation/authorValidationMessages.ts
@@ -1,8 +1,10 @@
 import { authorConstants } from "resources/constants/authorConstants";
 
 export const authorFailedValidation = {
-  FIRST_NAME_INVALID: `First name must only contain letters`,
+  FIRST_NAME_NOT_STRING_MESSAGE: "First name must be a string",
+  FIRST_NAME_INVALID_MESSAGE: `First name must only contain letters`,
   FIRST_NAME_BELOW_MIN_LENGTH_MESSAGE: `First name must be at least ${authorConstants.NAME_MIN_LENGTH} characters long`,
-  LAST_NAME_INVALID: `Last name must only contain letters`,
+  LAST_NAME_NOT_STRING_MESSAGE: `Last name must be a string`,
+  LAST_NAME_INVALID_MESSAGE: `Last name must only contain letters`,
   LAST_NAME_BELOW_MIN_LENGTH_MESSAGE: `Last name must be at least ${authorConstants.NAME_MIN_LENGTH} characters long`,
 };
