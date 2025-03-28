de327e7bc81a3f1a6f52df982fbb37dd2293f73e
geozi <yorgosaz@gmail.com>
2025-03-28 09:56:48 +0200

    feat: Added notFoundErrorClass and controller messages for author, book, and edition controllers.

---
+++ services/book-service/src/messages/response/authorControllerResponseMessages.ts
@@ -0,0 +1,5 @@
+export const authorControllerResponseMessages = {
+  AUTHOR_ADDED: "Successful author addition",
+  AUTHOR_UPDATED: "Successful author update",
+  AUTHOR_NOT_FOUND: "Author was not found",
+};
