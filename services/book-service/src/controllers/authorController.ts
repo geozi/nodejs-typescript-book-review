c8a1d1386d6d4ca8265f991e547f7be03a654a47
geozi <yorgosaz@gmail.com>
2025-03-28 10:08:55 +0200

    feat: Added authorController and apiVersionNumbers.

---
+++ services/book-service/src/controllers/authorController.ts
@@ -0,0 +1,39 @@
+import { ValidationError } from "class-validator";
+import { ServerError } from "errors/serverErrorClass";
+import { Request, Response } from "express";
+import { appLogger } from "../../logs/loggerConfigs";
+import { reqBodyToAuthor } from "mappers/authorMapper";
+import { authorControllerResponseMessages } from "messages/response/authorControllerResponseMessages";
+import { addAuthor } from "repositories/authorRepository";
+import { httpCodes } from "resources/codes/responseStatusCodes";
+import { apiVersionNumbers } from "resources/codes/apiVersionNumbers";
+
+export const callAuthorAddition = async (req: Request, res: Response) => {
+  try {
+    const newAuthor = reqBodyToAuthor(req);
+    const savedAuthor = await addAuthor(newAuthor);
+
+    res
+      .setHeader("X-api-version", apiVersionNumbers.VERSION_1_0)
+      .status(httpCodes.CREATED)
+      .json({
+        message: authorControllerResponseMessages.AUTHOR_ADDED,
+        data: savedAuthor,
+      });
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      appLogger.error(
+        `Author controller: ${callAuthorAddition.name} -> ValidationError detected and caught`
+      );
+
+      res.status(httpCodes.BAD_REQUEST).json(error.constraints);
+      return;
+    }
+
+    if (error instanceof ServerError) {
+      appLogger.error(
+        `Author controller: ${callAuthorAddition.name} -> ${error.name} detected and caught`
+      );
+    }
+  }
+};
