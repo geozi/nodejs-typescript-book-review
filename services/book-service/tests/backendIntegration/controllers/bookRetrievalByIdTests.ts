4fa775950e8117e4e56098f6eee29bee5dd5aaed
geozi <yorgosaz@gmail.com>
2025-03-29 19:20:02 +0200

    tests: Added bookRetrievalByIdTests test suite.

---
+++ services/book-service/tests/backendIntegration/controllers/bookRetrievalByIdTests.ts
@@ -0,0 +1,142 @@
+import assert from "assert";
+import { AppDataSource } from "config/dataSource";
+import { callBookRetrievalById } from "controllers/bookController";
+import { Book } from "entities/Book";
+import { Request, Response } from "express";
+import { bookControllerResponseMessages } from "messages/response/bookControllerResponseMessages";
+import { commonResponseMessages } from "messages/response/commonResponseMessages";
+import { apiVersionNumbers } from "resources/codes/apiVersionNumbers";
+import { httpCodes } from "resources/codes/responseStatusCodes";
+import sinon, { SinonSpy, SinonStub } from "sinon";
+import { validBookInputs } from "tests/testInputs";
+
+describe.only("Book retrieval by ID integration tests", () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let statusStub: SinonStub;
+  let jsonSpy: SinonSpy;
+  let setHeaderStub: SinonStub;
+  let findOneByStub: SinonStub;
+  let mockBook: Book;
+  let mockId: number;
+
+  describe("Positive scenarios", () => {
+    beforeEach(() => {
+      // Reset stubs and mocks
+      sinon.restore();
+
+      // Stubs
+      findOneByStub = sinon.stub(
+        AppDataSource.getRepository(Book),
+        "findOneBy"
+      );
+
+      res = {
+        setHeader: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        status: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      // Mocks
+      mockId = 1;
+      mockBook = new Book();
+      mockBook.id = mockId;
+      mockBook.title = validBookInputs.title;
+    });
+
+    it("ok (200)", async () => {
+      findOneByStub.resolves(mockBook);
+
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            id: mockId,
+          })
+        ),
+      };
+
+      await callBookRetrievalById(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+      setHeaderStub = res.setHeader as SinonStub;
+
+      assert.strictEqual(
+        setHeaderStub.calledWith(
+          "X-api-version",
+          apiVersionNumbers.VERSION_1_0
+        ),
+        true
+      );
+      assert.strictEqual(statusStub.calledWith(httpCodes.OK), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: bookControllerResponseMessages.BOOK_RETRIEVED,
+          data: mockBook,
+        }),
+        true
+      );
+    });
+  });
+
+  describe("Negative scenarios", () => {
+    beforeEach(() => {
+      // Reset stubs and mocks
+      sinon.restore();
+
+      // Stubs
+      findOneByStub = sinon.stub(
+        AppDataSource.getRepository(Book),
+        "findOneBy"
+      );
+
+      res = {
+        status: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      // Mocks
+      mockId = 1;
+      mockBook = new Book();
+      mockBook.id = mockId;
+      mockBook.title = validBookInputs.title;
+    });
+
+    it("server error (500)", async () => {
+      findOneByStub.rejects();
+
+      await callBookRetrievalById(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(
+        statusStub.calledWith(httpCodes.INTERNAL_SERVER_ERROR),
+        true
+      );
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: commonResponseMessages.SERVER_ERROR_MESSAGE,
+        }),
+        true
+      );
+    });
+
+    it("not found (404)", async () => {
+      findOneByStub.resolves(null);
+
+      await callBookRetrievalById(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(statusStub.calledWith(httpCodes.NOT_FOUND), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: bookControllerResponseMessages.BOOK_NOT_FOUND,
+        }),
+        true
+      );
+    });
+  });
+});
