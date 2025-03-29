7c61540f5d718c37b4aab2ae95ce64d10971b219
geozi <yorgosaz@gmail.com>
2025-03-29 19:11:16 +0200

    tests: Added more tests to bookRetrievalByTitleTests test suite.

--- services/book-service/tests/backendIntegration/controllers/bookRetrievalByTitleTests.ts
+++ services/book-service/tests/backendIntegration/controllers/bookRetrievalByTitleTests.ts
@@ -1,6 +1,12 @@
+import assert from "assert";
 import { AppDataSource } from "config/dataSource";
+import { callBookRetrievalByTitle } from "controllers/bookController";
 import { Book } from "entities/Book";
 import { Request, Response } from "express";
+import { bookControllerResponseMessages } from "messages/response/bookControllerResponseMessages";
+import { commonResponseMessages } from "messages/response/commonResponseMessages";
+import { apiVersionNumbers } from "resources/codes/apiVersionNumbers";
+import { httpCodes } from "resources/codes/responseStatusCodes";
 import sinon, { SinonSpy, SinonStub } from "sinon";
 import { validBookInputs } from "tests/testInputs";
 
@@ -33,6 +39,99 @@ describe("Book retrieval by title integration tests", () => {
       // Mocks
       mockBook = new Book();
       mockBook.title = validBookInputs.title;
+    });
+
+    it("ok (200)", async () => {
+      findOneByStub.resolves(mockBook);
+
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            title: validBookInputs.title,
+          })
+        ),
+      };
+
+      await callBookRetrievalByTitle(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+      setHeaderStub = res.setHeader as SinonStub;
+
+      assert.strictEqual(
+        setHeaderStub.calledWith(
+          "X-api-version",
+          apiVersionNumbers.VERSION_1_0
+        ),
+        true
+      );
+      assert.strictEqual(statusStub.calledWith(httpCodes.OK), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: bookControllerResponseMessages.BOOK_RETRIEVED,
+          data: mockBook,
+        }),
+        true
+      );
+    });
+  });
+
+  describe("Negative scenarios", async () => {
+    beforeEach(() => {
+      // Reset stubs and mocks
+      sinon.restore();
+
+      // Stubs
+      findOneByStub = sinon.stub(
+        AppDataSource.getRepository(Book),
+        "findOneBy"
+      );
+
+      res = {
+        status: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      // Mocks
+      mockBook = new Book();
+      mockBook.title = validBookInputs.title;
+    });
+
+    it("server error (500)", async () => {
+      findOneByStub.rejects();
+
+      await callBookRetrievalByTitle(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(
+        statusStub.calledWith(httpCodes.INTERNAL_SERVER_ERROR),
+        true
+      );
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: commonResponseMessages.SERVER_ERROR_MESSAGE,
+        }),
+        true
+      );
+    });
+
+    it("not found (404)", async () => {
+      findOneByStub.resolves(null);
+
+      await callBookRetrievalByTitle(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(statusStub.calledWith(httpCodes.NOT_FOUND), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: bookControllerResponseMessages.BOOK_NOT_FOUND,
+        }),
+        true
+      );
     });
   });
 });
