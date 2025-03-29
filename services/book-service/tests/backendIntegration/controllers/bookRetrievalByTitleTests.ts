828c76b06752caca0fbbb4fd1c9951cbd5681602
geozi <yorgosaz@gmail.com>
2025-03-29 18:17:30 +0200

    tests: Added describe block to bookRetrievalByTitleTests

--- services/book-service/tests/backendIntegration/controllers/bookRetrievalByTitleTests.ts
+++ services/book-service/tests/backendIntegration/controllers/bookRetrievalByTitleTests.ts
@@ -1,6 +1,8 @@
+import { AppDataSource } from "config/dataSource";
 import { Book } from "entities/Book";
 import { Request, Response } from "express";
-import { SinonSpy, SinonStub } from "sinon";
+import sinon, { SinonSpy, SinonStub } from "sinon";
+import { validBookInputs } from "../../testInputs";
 
 describe("Book retrieval by title integration tests", () => {
   let req: Partial<Request>;
@@ -10,4 +12,27 @@ describe("Book retrieval by title integration tests", () => {
   let setHeaderStub: SinonStub;
   let findOneByStub: SinonStub;
   let mockBook: Book;
+
+  describe("Positive scenarios", () => {
+    beforeEach(() => {
+      // Reset stubs and mocks
+      sinon.restore();
+
+      // Stubs
+      findOneByStub = sinon.stub(
+        AppDataSource.getRepository(Book),
+        "findOneBy"
+      );
+
+      res = {
+        setHeader: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        status: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      // Mocks
+      mockBook = new Book();
+      mockBook.title = validBookInputs.title;
+    });
+  });
 });
