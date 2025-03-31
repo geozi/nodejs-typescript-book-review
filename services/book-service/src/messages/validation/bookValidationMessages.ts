6affd7b9c82e9e327171f95c47900bd355829438
geozi <yorgosaz@gmail.com>
2025-03-31 12:16:08 +0300

    feat: Added express rules for Book model entity and extra messages to bookValidationMessages;

--- services/book-service/src/messages/validation/bookValidationMessages.ts
+++ services/book-service/src/messages/validation/bookValidationMessages.ts
@@ -2,8 +2,12 @@ import { bookConstants } from "resources/constants/bookConstants";
 import { Genre } from "resources/enum/Genre";
 
 export const bookFailedValidation = {
+  TITLE_REQUIRED_MESSAGE: "Title is a required field",
   TITLE_NOT_STRING_MESSAGE: "Title field must be a string",
   TITLE_BELOW_MIN_LENGTH_MESSAGE: `Title field must be at least ${bookConstants.TITLE_MIN_LENGTH} characters long`,
   TITLE_ABOVE_MAX_LENGTH_MESSAGE: `Title field must be no longer than ${bookConstants.TITLE_MAX_LENGTH} characters long`,
+  GENRE_REQUIRED_MESSAGE: "Genre is a required field",
   GENRE_INVALID_MESSAGE: `Genre field must be one of the following: ${Genre.DRAMA}, ${Genre.FICTION}, ${Genre.NON_FICTION}, ${Genre.POETRY}`,
+  BOOK_ID_REQUIRED_MESSAGE: "Book ID is a required field",
+  BOOK_ID_INVALID_MESSAGE: "Book ID must be a number",
 };
---
+++ services/book-service/src/middleware/rules/bookRules.ts
@@ -0,0 +1,79 @@
+import { check, ValidationChain } from "express-validator";
+import { bookFailedValidation } from "messages/validation/bookValidationMessages";
+import { bookConstants } from "resources/constants/bookConstants";
+import { genreArray } from "resources/enum/Genre";
+
+export const bookAdditionRules = (): ValidationChain[] => {
+  return [
+    check("title")
+      .notEmpty()
+      .withMessage(bookFailedValidation.TITLE_REQUIRED_MESSAGE)
+      .bail()
+      .isLength({ min: bookConstants.TITLE_MIN_LENGTH })
+      .withMessage(bookFailedValidation.TITLE_BELOW_MIN_LENGTH_MESSAGE)
+      .isLength({ max: bookConstants.TITLE_MAX_LENGTH })
+      .withMessage(bookFailedValidation.TITLE_ABOVE_MAX_LENGTH_MESSAGE),
+    check("genre")
+      .notEmpty()
+      .withMessage(bookFailedValidation.GENRE_REQUIRED_MESSAGE)
+      .bail()
+      .isIn(genreArray)
+      .withMessage(bookFailedValidation.GENRE_INVALID_MESSAGE),
+  ];
+};
+
+export const bookUpdateRules = (): ValidationChain[] => {
+  return [
+    check("id")
+      .notEmpty()
+      .withMessage(bookFailedValidation.BOOK_ID_REQUIRED_MESSAGE)
+      .bail()
+      .isNumeric()
+      .withMessage(bookFailedValidation.BOOK_ID_INVALID_MESSAGE),
+    check("title")
+      .optional()
+      .isLength({ min: bookConstants.TITLE_MIN_LENGTH })
+      .withMessage(bookFailedValidation.TITLE_BELOW_MIN_LENGTH_MESSAGE)
+      .isLength({ max: bookConstants.TITLE_MAX_LENGTH })
+      .withMessage(bookFailedValidation.TITLE_ABOVE_MAX_LENGTH_MESSAGE),
+    check("genre")
+      .optional()
+      .isIn(genreArray)
+      .withMessage(bookFailedValidation.GENRE_INVALID_MESSAGE),
+  ];
+};
+
+export const bookRetrievalByTitleRules = (): ValidationChain[] => {
+  return [
+    check("title")
+      .notEmpty()
+      .withMessage(bookFailedValidation.TITLE_REQUIRED_MESSAGE)
+      .bail()
+      .isLength({ min: bookConstants.TITLE_MIN_LENGTH })
+      .withMessage(bookFailedValidation.TITLE_BELOW_MIN_LENGTH_MESSAGE)
+      .isLength({ max: bookConstants.TITLE_MAX_LENGTH })
+      .withMessage(bookFailedValidation.TITLE_ABOVE_MAX_LENGTH_MESSAGE),
+  ];
+};
+
+export const bookRetrievalByIdRules = (): ValidationChain[] => {
+  return [
+    check("id")
+      .notEmpty()
+      .withMessage(bookFailedValidation.BOOK_ID_REQUIRED_MESSAGE)
+      .bail()
+      .isNumeric()
+      .withMessage(bookFailedValidation.BOOK_ID_INVALID_MESSAGE),
+  ];
+};
+
+export const bookRetrievalByGenre = (): ValidationChain[] => {
+  return [
+    check("genre")
+      .notEmpty()
+      .withMessage(bookFailedValidation.GENRE_REQUIRED_MESSAGE)
+      .bail()
+      .isIn(genreArray)
+      .withMessage(bookFailedValidation.GENRE_INVALID_MESSAGE),
+  ];
+};
