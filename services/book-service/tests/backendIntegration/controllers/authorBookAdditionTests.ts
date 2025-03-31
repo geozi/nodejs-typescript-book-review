fa290428167e5e81b9406b8f856cc6369c1bbef4
geozi <yorgosaz@gmail.com>
2025-03-31 17:34:04 +0300

    tests: Added extra tests to authorBookAdditionTests test suite.

--- services/book-service/tests/backendIntegration/controllers/authorBookAdditionTests.ts
+++ services/book-service/tests/backendIntegration/controllers/authorBookAdditionTests.ts
@@ -4,13 +4,16 @@ import { AppDataSource } from "db/dataSource";
 import { Author } from "entities/Author";
 import { Book } from "entities/Book";
 import { Request, Response } from "express";
+import { authorControllerResponseMessages } from "messages/response/authorControllerResponseMessages";
 import { bookAuthorControllerResponseMessages } from "messages/response/bookAuthorControllerResponseMessages";
+import { bookControllerResponseMessages } from "messages/response/bookControllerResponseMessages";
+import { commonResponseMessages } from "messages/response/commonResponseMessages";
 import { apiVersionNumbers } from "resources/codes/apiVersionNumbers";
 import { httpCodes } from "resources/codes/responseStatusCodes";
 import sinon, { SinonSpy, SinonStub } from "sinon";
 import { validAuthorInputs, validBookInputs } from "tests/testInputs";
 
-describe.only("AuthorBook addition integration tests", () => {
+describe("AuthorBook addition integration tests", () => {
   let req: Partial<Request>;
   let res: Partial<Response>;
   let statusStub: SinonStub;
@@ -93,6 +96,172 @@ describe.only("AuthorBook addition integration tests", () => {
       assert.strictEqual(
         jsonSpy.calledWith({
           message: bookAuthorControllerResponseMessages.RECORD_ADDED_MESSAGE,
+        }),
+        true
+      );
+    });
+  });
+
+  describe("Negative scenarios", () => {
+    beforeEach(() => {
+      // Reset stubs and mocks
+      sinon.restore();
+
+      // Stubs and spies
+      bookFindOneByStub = sinon.stub(
+        AppDataSource.getRepository(Book),
+        "findOneBy"
+      );
+      bookSaveStub = sinon.stub(AppDataSource.getRepository(Book), "save");
+      authorFindOneByStub = sinon.stub(
+        AppDataSource.getRepository(Author),
+        "findOneBy"
+      );
+      authorSaveStub = sinon.stub(AppDataSource.getRepository(Author), "save");
+
+      res = {
+        status: sinon.stub().callsFake(() => res) as unknown as SinonStub,
+        json: sinon.spy(),
+      };
+
+      // Mocks
+      mockBookId = "1";
+      mockAuthorId = "1";
+      mockBook = new Book();
+      mockBook.id = Number(mockBookId).valueOf();
+      mockBook.title = validBookInputs.title;
+      mockBook.genre = validBookInputs.genre;
+      mockAuthor = new Author();
+      mockAuthor.id = Number(mockAuthorId).valueOf();
+      mockAuthor.firstName = validAuthorInputs.firstName;
+      mockAuthor.lastName = validAuthorInputs.lastName;
+
+      req = {
+        body: JSON.parse(
+          JSON.stringify({
+            bookId: mockBookId,
+            authorId: mockAuthorId,
+          })
+        ),
+      };
+    });
+
+    it("server error (500) -> findOneBy (bookId) rejects", async () => {
+      bookFindOneByStub.rejects();
+
+      await callBookAuthorAddition(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(
+        statusStub.calledWith(httpCodes.INTERNAL_SERVER_ERROR),
+        true
+      );
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: commonResponseMessages.SERVER_ERROR_MESSAGE,
+        }),
+        true
+      );
+    });
+
+    it("server error (500) -> findOneBy (authorId) rejects", async () => {
+      bookFindOneByStub.resolves(mockBook);
+      authorFindOneByStub.rejects();
+
+      await callBookAuthorAddition(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(
+        statusStub.calledWith(httpCodes.INTERNAL_SERVER_ERROR),
+        true
+      );
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: commonResponseMessages.SERVER_ERROR_MESSAGE,
+        }),
+        true
+      );
+    });
+
+    it("server error (500) -> save (book) rejects", async () => {
+      bookFindOneByStub.resolves(mockBook);
+      authorFindOneByStub.resolves(mockAuthor);
+      bookSaveStub.rejects();
+
+      await callBookAuthorAddition(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(
+        statusStub.calledWith(httpCodes.INTERNAL_SERVER_ERROR),
+        true
+      );
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: commonResponseMessages.SERVER_ERROR_MESSAGE,
+        }),
+        true
+      );
+    });
+
+    it("server error (500) -> save (author) rejects", async () => {
+      bookFindOneByStub.resolves(mockBook);
+      authorFindOneByStub.resolves(mockAuthor);
+      bookSaveStub.resolves(mockBook);
+      authorSaveStub.rejects();
+
+      await callBookAuthorAddition(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(
+        statusStub.calledWith(httpCodes.INTERNAL_SERVER_ERROR),
+        true
+      );
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: commonResponseMessages.SERVER_ERROR_MESSAGE,
+        }),
+        true
+      );
+    });
+
+    it("not found (404) -> findOneBy (bookId) returns null", async () => {
+      bookFindOneByStub.resolves(null);
+
+      await callBookAuthorAddition(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(statusStub.calledWith(httpCodes.NOT_FOUND), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: bookControllerResponseMessages.BOOK_NOT_FOUND,
+        }),
+        true
+      );
+    });
+
+    it("not found (404) -> findOneBy (authorId) returns null", async () => {
+      bookFindOneByStub.resolves(mockBook);
+      authorFindOneByStub.resolves(null);
+
+      await callBookAuthorAddition(req as Request, res as Response);
+
+      statusStub = res.status as SinonStub;
+      jsonSpy = res.json as SinonSpy;
+
+      assert.strictEqual(statusStub.calledWith(httpCodes.NOT_FOUND), true);
+      assert.strictEqual(
+        jsonSpy.calledWith({
+          message: authorControllerResponseMessages.AUTHOR_NOT_FOUND,
         }),
         true
       );
