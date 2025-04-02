eba937e387fc05730acfa59bd632d542e8ed268e
geozi <yorgosaz@gmail.com>
2025-04-02 18:09:03 +0300

    docs: Added TypeDoc comments to src/interfaces.

--- services/book-service/src/interfaces/IAuthorUpdate.ts
+++ services/book-service/src/interfaces/IAuthorUpdate.ts
@@ -1,4 +1,26 @@
+/**
+ * IAuthorUpdate interface.
+ * @module src/interfaces/IAuthorUpdate
+ */
+
+/**
+ * Represents a data object used to update the
+ * information of an author.
+ *
+ * @interface
+ * @property {string} [firstName] - (Optional) The first name of an author.
+ * @property {string} [lastName] - (Optional) The last name of an author.
+ */
 export interface IAuthorUpdate {
+  /**
+   * (Optional) The first name of an author.
+   * @type {string}
+   */
   firstName?: string;
+
+  /**
+   * (Optional) The last name of an author.
+   * @type {string}
+   */
   lastName?: string;
 }
