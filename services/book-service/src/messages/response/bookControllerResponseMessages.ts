49b3baddbfe3637085e80078a4c3ad1bb7d4d352
geozi <yorgosaz@gmail.com>
2025-04-02 19:02:29 +0300

    refactor: Added _MESSAGE to the end of response message identifiers.

--- services/book-service/src/messages/response/bookControllerResponseMessages.ts
+++ services/book-service/src/messages/response/bookControllerResponseMessages.ts
@@ -7,47 +7,47 @@
  * Contains HTTP response messages sent by the book controller.
  *
  * @type {object}
- * @property {string} BOOK_ADDED - Message sent when the information of a new book is successfully added to database.
- * @property {string} BOOK_UPDATED - Message sent when the information of a book is successfully updated.
- * @property {string} BOOK_NOT_FOUND - Message sent when the information of a book is not found in the database.
- * @property {string} BOOK_S_NOT_FOUND - Message sent when the information of multiple books is not found in the database.
- * @property {string} BOOK_RETRIEVED - Message sent when the information of a book is found and successfully retrieved from database.
- * @property {string} BOOK_S_RETRIEVED - Message sent when the information of multiple books is found and successfully retrieved from database.
+ * @property {string} BOOK_ADDED_MESSAGE - Message sent when the information of a new book is successfully added to database.
+ * @property {string} BOOK_UPDATED_MESSAGE - Message sent when the information of a book is successfully updated.
+ * @property {string} BOOK_NOT_FOUND_MESSAGE - Message sent when the information of a book is not found in the database.
+ * @property {string} BOOK_S_NOT_FOUND_MESSAGE - Message sent when the information of multiple books is not found in the database.
+ * @property {string} BOOK_RETRIEVED_MESSAGE - Message sent when the information of a book is found and successfully retrieved from database.
+ * @property {string} BOOK_S_RETRIEVED_MESSAGE - Message sent when the information of multiple books is found and successfully retrieved from database.
  */
 export const bookControllerResponseMessages = {
   /**
    * Message sent when the information of a new book is successfully added to database.
    * @type {string}
    */
-  BOOK_ADDED: "Successful book addition",
+  BOOK_ADDED_MESSAGE: "Successful book addition",
 
   /**
    * Message sent when the information of a book is successfully updated.
    * @type {string}
    */
-  BOOK_UPDATED: "Successful book update",
+  BOOK_UPDATED_MESSAGE: "Successful book update",
 
   /**
    * Message sent when the information of a book is not found in the database.
    * @type {string}
    */
-  BOOK_NOT_FOUND: "Book was not found",
+  BOOK_NOT_FOUND_MESSAGE: "Book was not found",
 
   /**
    * Message sent when the information of multiple books is not found in the database.
    * @type {string}
    */
-  BOOK_S_NOT_FOUND: "Books were not found",
+  BOOK_S_NOT_FOUND_MESSAGE: "Books were not found",
 
   /**
    * Message sent when the information of a book is found and successfully retrieved from database.
    * @type {string}
    */
-  BOOK_RETRIEVED: "Successful book retrieval",
+  BOOK_RETRIEVED_MESSAGE: "Successful book retrieval",
 
   /**
    * Message sent when the information of multiple books is found and successfully retrieved from database.
    * @type {string}
    */
-  BOOK_S_RETRIEVED: "Successful retrieval of books",
+  BOOK_S_RETRIEVED_MESSAGE: "Successful retrieval of books",
 };
