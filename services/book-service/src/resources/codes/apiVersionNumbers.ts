8e4a148a9b38789a38cb2c0e0b688ec9766946ba
geozi <yorgosaz@gmail.com>
2025-04-03 11:59:00 +0300

    docs: Added TypeDoc comments to src/resources/codes.

--- services/book-service/src/resources/codes/apiVersionNumbers.ts
+++ services/book-service/src/resources/codes/apiVersionNumbers.ts
@@ -1,3 +1,14 @@
+/**
+ * API version numbers.
+ * @module src/resources/codes/apiVersionNumbers
+ */
+
+/**
+ * Contains API version numbers.
+ *
+ * @type {object}
+ * @property {string} VERSION_1_0 - A string for version 1.0.
+ */
 export const apiVersionNumbers = {
   VERSION_1_0: "1.0",
 };
