d7ff4c4e5a721c27938776b235431fed1059007f
geozi <yorgosaz@gmail.com>
2025-04-03 17:17:44 +0300

    docs: Added TypeDoc comments to validationRegExp.

--- services/book-service/src/resources/regexp/validationRegExp.ts
+++ services/book-service/src/resources/regexp/validationRegExp.ts
@@ -1,1 +1,10 @@
+/**
+ * Regular expressions used in validation processes.
+ * @module src/resources/regExp/validationRegExp
+ */
+
+/**
+ * Regular expression used in checking only-letter string validity.
+ * @type {RegExp}
+ */
 export const ALPHABETIC_REGEX = new RegExp(/^[A-Za-z]+$/);
