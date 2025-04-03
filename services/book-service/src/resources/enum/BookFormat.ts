1deb8032ec2814577e5a01b52debb66316213e27
geozi <yorgosaz@gmail.com>
2025-04-03 17:15:37 +0300

    docs: Added TypeDoc comments to src/resources/enum.

--- services/book-service/src/resources/enum/BookFormat.ts
+++ services/book-service/src/resources/enum/BookFormat.ts
@@ -1,6 +1,34 @@
+/**
+ * BookFormat enums.
+ * @module src/resources/enum/BookFormat
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { EnumMember } from "typescript";
+
+/**
+ * Contains {@link EnumMember} corresponding to book formats.
+ *
+ * @readonly
+ * @enum
+ */
 export enum BookFormat {
+  /**
+   * Hardcover format.
+   * @readonly
+   * @type {EnumMember}
+   */
   HARDCOVER = "Hardcover",
+
+  /**
+   * Ebook format.
+   * @readonly
+   * @type {EnumMember}
+   */
   EBOOK = "Ebook",
 }
 
+/**
+ * Book format array.
+ * @type {BookFormat[]}
+ */
 export const bookFormatArray = [BookFormat.HARDCOVER, BookFormat.EBOOK];
--- services/book-service/src/resources/enum/Genre.ts
+++ services/book-service/src/resources/enum/Genre.ts
@@ -1,10 +1,52 @@
+/**
+ * Genre enums.
+ * @module src/resources/enum/Genre
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { EnumMember } from "typescript";
+
+/**
+ * Contains {@link EnumMember} corresponding to book genres.
+ *
+ * @readonly
+ * @enum
+ */
 export enum Genre {
+  /**
+   * Fiction genre.
+   * @readonly
+   * @type {EnumMember}
+   */
   FICTION = "Fiction",
+
+  /**
+   * Non-fiction genre.
+   * @readonly
+   * @type {EnumMember}
+   */
   NON_FICTION = "Non-fiction",
+
+  /**
+   * Poetry genre.
+   * @readonly
+   * @type {EnumMember}
+   */
   POETRY = "Poetry",
+
+  /**
+   * Drama genre.
+   * @readonly
+   * @type {EnumMember}
+   */
   DRAMA = "Drama",
 }
 
+/**
+ * Genre array.
+ * @readonly
+ * @type {Genre[]}
+ */
 export const genreArray = [
   Genre.FICTION,
   Genre.NON_FICTION,
