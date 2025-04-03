128969e0e02a632c9655abfb941aaab55f749f3b
geozi <yorgosaz@gmail.com>
2025-04-03 11:10:19 +0300

    docs: Added TypeDoc comments in src/middleware

--- services/book-service/src/middleware/catchers/expressErrorCatcher.ts
+++ services/book-service/src/middleware/catchers/expressErrorCatcher.ts
@@ -1,13 +1,25 @@
+/**
+ * Express validation error catcher.
+ * @module src/middleware/catchers/expressErrorCatcher
+ */
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { appLogger } from "logs/loggerConfigs";
 import { httpCodes } from "resources/codes/responseStatusCodes";
 
+/**
+ * Handles express validation errors.
+ *
+ * @param {Request} req - An HTTP request.
+ * @param {Response} res - An HTTP response.
+ * @param {NextFunction} next - A function moving program logic to the next step of a middleware path.
+ * @returns { Promise<void>} A promise that resolves to void.
+ */
 export const catchExpressValidationErrors = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const expressErrors = validationResult(req);
   if (!expressErrors.isEmpty()) {
     const errorMessage = expressErrors.array().map((err) => ({
