30d22610aaee45f7f2557072ff215c468a71e6e0
geozi <yorgosaz@gmail.com>
2025-04-04 13:28:50 +0300

    fix: Removed data field from axios options in requestForwarder and added next() to anonymous function in authorRouter.post()

--- services/book-service/src/routes/authorRoutes.ts
+++ services/book-service/src/routes/authorRoutes.ts
@@ -4,7 +4,7 @@ import {
   callAuthorRetrievalById,
   callAuthorUpdate,
 } from "controllers/authorController";
-import { Request, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { catchExpressValidationErrors } from "middleware/catchers/expressErrorCatcher";
 import {
   authorAdditionRules,
@@ -22,8 +22,9 @@ authorRouter.get(
 );
 authorRouter.post(
   "/",
-  (req: Request) => {
+  (req: Request, _res: Response, next: NextFunction) => {
     req.body.role = RoleType.Admin;
+    next();
   },
   forwardToAccountService,
   ...authorAdditionRules(),
