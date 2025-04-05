567cd6a42f607fdcedad36bb586adb68c25bd95f
geozi <yorgosaz@gmail.com>
2025-04-05 11:47:11 +0300

    refactor: Replaced anonymous function handler with assignRoleType handler in bookAuthorRoutes.

--- services/book-service/src/routes/bookAuthorRoutes.ts
+++ services/book-service/src/routes/bookAuthorRoutes.ts
@@ -4,18 +4,16 @@
  */
 import { forwardToAccountService } from "auth/requestForwarder";
 import { callBookAuthorAddition } from "controllers/bookAuthorController";
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { catchExpressValidationErrors } from "middleware/catchers/expressErrorCatcher";
+import { assignRoleType } from "middleware/handlers/roleTypeAssignment";
 import { authorBookAdditionRules } from "middleware/rules/authorBookRules";
 import { RoleType } from "resources/enum/RoleType";
 
 export const bookAuthorRouter = Router();
 bookAuthorRouter.post(
   "/",
-  async (req: Request, _res: Response, next: NextFunction) => {
-    req.body.role = RoleType.Admin;
-    next();
-  },
+  assignRoleType(RoleType.Admin),
   forwardToAccountService,
   ...authorBookAdditionRules(),
   catchExpressValidationErrors,
