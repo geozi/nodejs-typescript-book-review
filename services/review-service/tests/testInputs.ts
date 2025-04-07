34625384e7d1a292e99ae47a2d0a728c00d7b927
geozi <yorgosaz@gmail.com>
2025-04-07 18:21:01 +0300

    tests: Added testInputs.

---
+++ services/review-service/tests/testInputs.ts
@@ -0,0 +1,9 @@
+export const validReviewInputs = {
+  subject: "This is a valid review",
+  description: "The book was a great read, ideal for relaxing times.",
+  book: {
+    id: 1,
+  },
+};
+
+export const invalidReviewInputs = {};
