5a1cc00f8fd8f264e51a6f4ed2a5112295864c18
geozi <yorgosaz@gmail.com>
2025-04-07 18:29:04 +0300

    tests: Added invalidReviewInputs to testInputs.

--- services/review-service/tests/testInputs.ts
+++ services/review-service/tests/testInputs.ts
@@ -6,4 +6,9 @@ export const validReviewInputs = {
   },
 };
 
-export const invalidReviewInputs = {};
+export const invalidReviewInputs = {
+  SUBJECT_TOO_SHORT: "S",
+  SUBJECT_TOO_LONG: `This subject title is incredibly long and rambling, exceeding the one hundred character limit without a care in the world!`,
+  DESCRIPTION_TOO_SHORT: "Short!",
+  DESCRIPTION_TOO_LONG: `This description is far too verbose and excessive, stretching on and on past the three hundred character limit that has been imposed for validation purposes. It seems to have no regard for constraints, and thus should undoubtedly fail validation criteria due to its sheer length and disregard for brevity.`,
+};
