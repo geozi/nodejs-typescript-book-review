abb2b8d12d459cd61e63967c5c8ca4a9b16346dd
geozi <yorgosaz@gmail.com>
2025-04-12 11:41:56 +0300

    feat: Added extra functionality to reviewController; added REVIEW_RETRIEVED_MESSAGE and REVIEW_S_RETRIEVED_MESSAGE to reviewResponseMessages.

--- services/review-service/src/messages/response/reviewResponseMessages.ts
+++ services/review-service/src/messages/response/reviewResponseMessages.ts
@@ -3,4 +3,6 @@ export const reviewResponseMessages = {
   REVIEW_UPDATE_MESSAGE: "Successful review update",
   REVIEW_NOT_FOUND_MESSAGE: "Review was not found",
   REVIEW_S_NOT_FOUND_MESSAGE: "No reviews were found",
+  REVIEW_RETRIEVED_MESSAGE: "Successful review retrieval",
+  REVIEW_S_RETRIEVED_MESSAGE: "Successful retrieval of reviews",
 };
