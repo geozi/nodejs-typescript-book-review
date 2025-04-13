355950e88e2c74a4a3acbf63a857e9bb38a5bba1
geozi <yorgosaz@gmail.com>
2025-04-13 19:04:15 +0300

    docs: Added TypeDoc comments to src/middleware.

--- services/review-service/src/middleware/catchers/expressErrorCatcher.ts
+++ services/review-service/src/middleware/catchers/expressErrorCatcher.ts
@@ -1,9 +1,21 @@
+/**
+ * Express validation error catcher.
+ * @module src/middleware/catchers/expressErrorCatcher
+ */
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { appLogger } from "logs/loggerConfig";
 import { commonResponseMessages } from "messages/response/commonResponseMessages";
 import { httpCodes } from "resources/codes/responseStatusCodes";
 
+/**
+ * Handles express validation errors.
+ *
+ * @param {Request} req - An HTTP request.
+ * @param {Response} res - An HTTP response.
+ * @param {NextFunction} next - A function moving program logic to the next step of a middleware path.
+ * @returns { Promise<void>} A promise that resolves to void.
+ */
 export const catchExpressValidationErrors = async (
   req: Request,
   res: Response,
@@ -19,12 +31,10 @@ export const catchExpressValidationErrors = async (
       `${catchExpressValidationErrors.name} -> Express validation errors detected and caught`
     );
 
-    res
-      .status(httpCodes.BAD_REQUEST)
-      .json({
-        message: commonResponseMessages.BAD_REQUEST_MESSAGE,
-        errors: errorMessage,
-      });
+    res.status(httpCodes.BAD_REQUEST).json({
+      message: commonResponseMessages.BAD_REQUEST_MESSAGE,
+      errors: errorMessage,
+    });
     return;
   }
 
