8e9c1d548c008366b93bab4d4e529aae598d1da0
geozi <yorgosaz@gmail.com>
2025-04-13 17:56:05 +0300

    docs: Added TypeDoc comments to src/errors.

--- services/review-service/src/errors/notFoundErrorClass.ts
+++ services/review-service/src/errors/notFoundErrorClass.ts
@@ -1,7 +1,19 @@
+/**
+ * NotFoundError class.
+ * @module src/errors/notFoundErrorClass
+ */
 import { httpCodes } from "resources/codes/responseStatusCodes";
 import { AbstractError } from "./abstractErrorClass";
 
+/**
+ * Triggered when the requested record(s) is/are not found (404).
+ * @extends {AbstractError}
+ */
 export class NotFoundError extends AbstractError {
+  /**
+   * Creates an instance of NotFoundError.
+   * @param {string} message The error message.
+   */
   constructor(message: string) {
     super(httpCodes.NOT_FOUND, message);
     this.name = "NotFoundError";
