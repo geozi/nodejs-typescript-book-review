2353aedfaf29e629436624d617c1579e64afc91c
geozi <yorgosaz@gmail.com>
2025-04-13 19:34:50 +0300

    docs: Added TypeDoc comments to src/resources.

--- services/review-service/src/resources/enum/RoleType.ts
+++ services/review-service/src/resources/enum/RoleType.ts
@@ -1,4 +1,26 @@
-export enum RoleType {
+/**
+ * RoleType enums.
+ * @module src/resources/enum/RoleType
+ */
+
+/**
+ * Contains enums corresponding to user roles.
+ *
+ * @readonly
+ * @enum
+ */
+export const enum RoleType {
+  /**
+   * Admin role.
+   * @readonly
+   * @type {EnumMember}
+   */
   Admin = "Admin",
+
+  /**
+   * User role.
+   * @readonly
+   * @type {EnumMember}
+   */
   User = "User",
 }
