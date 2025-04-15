09dfd1e2782f305c24ac767f95cfc7d7a0112773
geozi <yorgosaz@gmail.com>
2025-04-15 11:50:33 +0300

    feat: Added response logic to assignRoleType function and REQUEST_BODY_REQUIRED_MESSAGE  to commonResponseMessages in book-service.

--- services/book-service/src/middleware/handlers/roleTypeAssignment.ts
+++ services/book-service/src/middleware/handlers/roleTypeAssignment.ts
@@ -3,16 +3,24 @@
  * @module src/middleware/handlers/roleTypeAssignment
  */
 import { NextFunction, Request, Response } from "express";
+import { commonResponseMessages } from "messages/response/commonResponseMessages";
+import { httpCodes } from "resources/codes/responseStatusCodes";
 import { RoleType } from "resources/enum/RoleType";
 
 /**
  * Adds a {@link RoleType} enum to the body of an HTTP request.
  *
  * @param {RoleType} role - A role assigned to each user.
- * @returns {Promise<void>} A promise that results to void.
+ * @returns {void}
  */
 export const assignRoleType = (role: RoleType) => {
-  return async (req: Request, _res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.body) {
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: commonResponseMessages.REQUEST_BODY_REQUIRED_MESSAGE,
+      });
+      return;
+    }
     req.body.role = role;
     next();
   };
